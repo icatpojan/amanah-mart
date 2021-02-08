afcf9b65683e7fa452d7303c392f505bc4219bd5
icatpojan <irsyadfauzangg@gmail.com>
2021-02-08 16:46:09 +0700

    membuat api verivikasi dan forgot

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,8 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/email/resend', 'Api\VerificationController@resend')->name('verification.resend'); //kirim email verivikasi
+Route::get('/email/verify/{id}/{hash}', 'Api\VerificationController@verify')->name('verification.verify'); //kirim email verivikasi
+
+//route reset password
+Route::post('password/email', 'Api\ForgotPasswordController@forgot'); //mengirim email reset password
+Route::post('password/reset', 'Api\ForgotPasswordController@reset');
+
+
+Route::group(['namespace' => 'Api'], function () {
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@login');
+});
+
+Route::group(['namespace' => 'Api', 'middleware' => ['jwt.verify']], function () {
+    Route::get('bookall', 'BookController@bookAuth')->middleware('jwt.verify');
 });
