57b5c3d62d8b73119b8367f0dcf1d2c4772093d4
icatpojan <irsyadfauzangg@gmail.com>
2021-02-08 11:13:07 +0700

    first

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password'  => Hash::make('123123'),
+            'email_verified_at' => now(),
+            'role_id'   => 5
+        ]);
+
+    }
+}
