b1950fe8bb47f6dc4a0ac8056ab037ebd222c860
icatpojan <irsyadfauzangg@gmail.com>
2021-02-19 20:34:17 +0700

    fix bug

--- app/Http/Controllers/Api/PenjualanController.php
+++ app/Http/Controllers/Api/PenjualanController.php
@@ -15,7 +15,7 @@ class PenjualanController extends Controller
 {
     public function index()
     {
-        $Penjualan = Penjualan::where('user_id', Auth::user()->id)->where('status', 0)->latest()->first();
+        $Penjualan = Penjualan::where('id_kasir', Auth::user()->id)->where('status', 0)->latest()->first();
         $Cart = Cart::where('Penjualan_id', $Penjualan->id)->where('status', 0)->get();
         if ($Cart == '[]') {
             return $this->sendResponse('Failed', 'data kosong', null, 404);
